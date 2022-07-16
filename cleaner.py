12fe63e487d85b3e318cd5863cb1684de7214711
divyanshipatel123 <divyanshipatel2008apr13@gmail.com>
2022-07-16 09:38:17 +0530

    modification to remove values with ? mark

--- cleaner.py
+++ cleaner.py
@@ -1,10 +1,12 @@
 import csv 
 import pandas as pd
-df = pd.read_csv("main.csv")
+missing_val = ["?"]
+df = pd.read_csv("main.csv" , na_values=missing_val)
 print(df.shape)
 df.drop(["Unnamed: 0","Star_Name", "Star_Mass", "Star_Radius" , "Distance" , "Star_luminosity"] , axis=1 , inplace=True)
 final = df.dropna()
 final.reset_index(drop=True,inplace = True)
 print(final)
 print(final.info())
+print(df.dtypes)
 final.to_csv("final.csv" , index=False)
